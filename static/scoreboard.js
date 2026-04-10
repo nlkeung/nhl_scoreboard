4084e634b622078f251e862efcfdd2ba556e7c9b
nlkeung <nlkeung@yahoo.com>
2026-04-10 01:17:13 -0400

    Sort incoming data, then pass into display_scoreboard()

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,11 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      let data = result.scoreboard
+      data.sort(function(a, b) {
+        return b.score - a.score || a.name - b.name
+      })
+      display_scoreboard(result.scoreboard)
     },
     error: function(request, status, error){
         console.log("Error");
